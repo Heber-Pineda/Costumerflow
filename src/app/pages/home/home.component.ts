84b50f3cdaf413c194ed7b825a80cda7950ff3b0
Heber Pineda <157323289+Heber-Pineda@users.noreply.github.com>
2024-08-31 20:21:07 -0600

    Proyecto Citas

---
+++ src/app/pages/home/home.component.ts
@@ -0,0 +1,40 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from 'src/app/authentication/services/auth.service';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./logo.css'],
+  styles: [`
+    mat-sidenav-container {
+      background-color: #f2f2f2; /* Color gris claro */
+    }
+  `]
+})
+export class HomeComponent implements OnInit {
+  isAdmin: boolean = false;
+  isDoctor: boolean = false;
+  isUser: boolean = false;
+
+  constructor(public authService: AuthService, private router: Router) {}
+
+  ngOnInit(): void {
+    if (this.authService.currentUser) {
+      if (this.authService.currentUser.rol == 'ADMIN') this.isAdmin = true;
+    }
+
+    if (this.authService.currentUser) {
+      if (this.authService.currentUser.rol == 'DOCTOR') this.isDoctor = true;
+    }
+
+    if (this.authService.currentUser) {
+      if (this.authService.currentUser.rol == 'USER') this.isUser = true;
+    }
+  }
+
+  logout() {
+    this.authService.logout();
+    this.router.navigateByUrl('/authentication');
+  }
+}
